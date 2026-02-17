03b46ed0b350ca3c64496bf7d6e8afda4c53e1d4
André Neves <andrerfneves@protonmail.com>
2026-02-16 16:34:28 -0800

    Add Breez logo beside Vinteum in partners section

--- components/partners.tsx
+++ components/partners.tsx
@@ -8,7 +8,7 @@ function Partners() {
         <Badge variant="outline" className="mb-4 bg-pink-700">
           Global Partners
         </Badge>
-        <div className="flex justify-center mt-8">
+        <div className="mt-8 flex flex-col items-center justify-center gap-8 md:flex-row md:gap-12">
           <Link
             href="https://vinteum.org"
             target="_blank"
@@ -17,6 +17,18 @@ function Partners() {
             <img
               src="/partners/vinteum.png"
               alt="Vinteum"
+              className="h-12 md:h-16 w-auto hover:opacity-80 transition-opacity"
+            />
+          </Link>
+
+          <Link
+            href="https://breez.technology"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/partners/breez.svg"
+              alt="Breez"
               className="h-12 md:h-16 w-auto hover:opacity-80 transition-opacity"
             />
           </Link>
